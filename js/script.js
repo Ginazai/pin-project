1ab769a8ab7d38c051df62f79c2b4dac942a6cb0
Ginazai <rafaeldc1300@gmail.com>
2023-09-07 22:42:40 -0500

    stetic (front-end) improvements

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
+function classOnResize(elem, size, newClass) {
+	if ($(window).width() <= size) {
+		$(elem).addClass(newClass);
+	} else {
+		$(elem).removeClass(newClass);
+	}
+}
+
 $(window).on('resize', function() {
-	if ($(window).width() <= 760) {
-		$('#my-footer').addClass('fixed-bottom');
-	} else {$('#my-footer').removeClass('fixed-bottom');}
-	
+	classOnResize('.pin-carousel-item', 900, 'text-center');
+	classOnResize('.pins-carousel-indicators', 767, 'visually-hidden');	
+	classOnResize('.dropdown-menu', 991, 'glass rounded-2');
+	classOnResize('#glass-piece-6', 800, 'd-none');
+	classOnResize('#glass-piece-4', 380, 'd-none');
+	classOnResize('#glass-piece-1', 325, 'd-none');
+	classOnResize('#glass-piece-2', 195, 'd-none');
+	classOnResize('#glass-piece-3', 195, 'd-none');
+	classOnResize('#glass-piece-5', 175, 'd-none');
 });
 
 $(document).ready(function() {
-	if ($(window).width() <= 760) {
-		$('#my-footer').addClass('fixed-bottom');
-	} else {$('#my-footer').removeClass('fixed-bottom');}
+	classOnResize('.pin-carousel-item', 900, 'text-center');
+	classOnResize('.pins-carousel-indicators', 767, 'visually-hidden');
+	classOnResize('.dropdown-menu', 991, 'glass rounded-2');
+	classOnResize('#glass-piece-6', 800, 'd-none');
+	classOnResize('#glass-piece-4', 380, 'd-none');
+	classOnResize('#glass-piece-1', 325, 'd-none');
+	classOnResize('#glass-piece-2', 195, 'd-none');
+	classOnResize('#glass-piece-3', 195, 'd-none');
+	classOnResize('#glass-piece-5', 175, 'd-none');
 });
